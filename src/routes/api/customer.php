0bc2595b9e305d60f5129322de96fb33f9427286
Woo Woo <aron_118@yahoo.com.hk>
2023-06-27 14:51:19 +0800

    getAuctionHistory

--- src/routes/api/customer.php
+++ src/routes/api/customer.php
@@ -30,3 +30,28 @@ Route::group(
         Route::get('/health-check', [$defaultController, 'healthCheck']);
     }
 );
+
+/*
+|--------------------------------------------------------------------------
+| Product related
+|--------------------------------------------------------------------------
+*/
+
+// STORE
+Route::group(
+    ['prefix' => '/stores/{store_id}/'],
+    function () {
+
+        // PRODUCT_MANAGEMENT
+        Route::group(
+            ['prefix' => 'product-management'],
+            function () {
+                $defaultController = ProductManagementController::class;
+
+                Route::get('/products/filter', [$defaultController, 'filterProductsByCategories'])->middleware(['pagination']);
+
+                Route::get('/products/{product_variant_id}/history', [$defaultController, 'getAuctionHistory'])->middleware(['pagination']);
+            }
+        );
+    }
+);
