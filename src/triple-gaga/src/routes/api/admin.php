afa61ec5d2825f4352da3f35d783b429d9f847b7
fai <haystack312@gmail.com>
2023-07-20 12:05:08 +0800

    init, working

---
+++ src/triple-gaga/src/routes/api/admin.php
@@ -0,0 +1,32 @@
+<?php
+
+// Default Imports
+use Illuminate\Support\Facades\Route;
+
+use StarsNet\Project\TripleGaga\App\Http\Controllers\Admin\TestingController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Development Uses
+|--------------------------------------------------------------------------
+*/
+
+Route::group(
+    ['prefix' => 'tests'],
+    function () {
+        $defaultController = TestingController::class;
+
+        Route::get('/health-check', [$defaultController, 'healthCheck']);
+    }
+);
---
+++ src/triple-gaga/src/routes/api/customer.php
@@ -0,0 +1,58 @@
+<?php
+
+// Default Imports
+use Illuminate\Support\Facades\Route;
+
+use StarsNet\Project\TripleGaga\App\Http\Controllers\Customer\TestingController;
+use StarsNet\Project\TripleGaga\App\Http\Controllers\Customer\ProductManagementController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Development Uses
+|--------------------------------------------------------------------------
+*/
+
+Route::group(
+    ['prefix' => 'tests'],
+    function () {
+        $defaultController = TestingController::class;
+
+        Route::get('/health-check', [$defaultController, 'healthCheck']);
+    }
+);
+
+/*
+|--------------------------------------------------------------------------
+| Product related
+|--------------------------------------------------------------------------
+*/
+
+// STORE
+Route::group(
+    ['prefix' => '/stores/{store_id}/'],
+    function () {
+
+        // PRODUCT_MANAGEMENT
+        Route::group(
+            ['prefix' => 'product-management'],
+            function () {
+                $defaultController = ProductManagementController::class;
+
+                Route::get('/products/filter', [$defaultController, 'filterProductsByCategories'])->middleware(['pagination']);
+
+                Route::get('/products/{product_variant_id}/history', [$defaultController, 'getAuctionHistory'])->middleware(['pagination']);
+            }
+        );
+    }
+);
