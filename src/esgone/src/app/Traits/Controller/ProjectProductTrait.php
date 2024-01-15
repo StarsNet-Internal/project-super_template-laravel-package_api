30c3579591b328bdb5e2dd941fb09535ba03a198
Woo <aron_118@yahoo.com.hk>
2024-01-15 17:01:16 +0800

    filterProductsByCategories returns all variants

--- src/esgone/src/app/Traits/Controller/ProjectProductTrait.php
+++ src/esgone/src/app/Traits/Controller/ProjectProductTrait.php
@@ -6,117 +6,52 @@ namespace StarsNet\Project\Esgone\App\Traits\Controller;
 
 use App\Constants\Model\ProductVariantDiscountType;
 use App\Models\Product;
-use App\Models\DiscountTemplate;
-use App\Traits\Utils\RoundingTrait;
 use Illuminate\Support\Facades\Auth;
 
 trait ProjectProductTrait
 {
-    use RoundingTrait;
-
     private function getProductsInfoByEagerLoading(array $productIDs)
     {
-        $storeID = $this->store->_id;
-
-        $products = Product::with([
-            'variants' => function ($productVariant) {
-                $productVariant->with([
-                    'discounts' => function ($discount) {
-                        $discount->applicableForCustomer()->select('product_variant_id', 'type', 'value', 'start_datetime', 'end_datetime');
-                    },
-                ])->statusActive()->get();
-            },
-            'wishlistItems',
-            'reviews',
-            'warehouseInventories'
-        ])
-            ->find($productIDs)
-            ->append(['first_product_variant_id', 'price', 'point']);
-
-        $globalDiscounts = DiscountTemplate::where('start_datetime', '<', now())
-            ->where('end_datetime', '>=', now())
-            ->where('status', 'ACTIVE')
-            ->get()
-            ->makeVisible(['x', 'y'])
-            ->makeHidden(['product_variant_x', 'product_variant_y']);
-
-        foreach ($products as $product) {
-            $collectedReviews = collect($product->reviews);
-            $hasVariants = count($product['variants']) > 0;
-            $key = array_search(0, array_column($product['variants']->toArray(), 'price'));
-            $freeVariant = $product['variants'][$key];
-
-            $hasLocalDiscount = $hasVariants ? count($freeVariant['discounts']) > 0 : false;
-
-            if ($hasVariants) {
-                $matchingDiscounts = $globalDiscounts->filter(function ($discount) use ($storeID, $product) {
-                    return in_array($storeID, $discount->store_ids) && $discount['x.product_variant_id'] === $product->first_product_variant_id;
-                });
-                $product['global_discount'] = count($matchingDiscounts) > 0 ? $matchingDiscounts[0] : null;
-            } else {
-                $product['global_discount'] = null;
-            }
-
-            $product['rating'] = $collectedReviews->avg('rating') ?? null;
-            $product['review_count'] = $collectedReviews->count() ?? 0;
-
-            $product['inventory_count'] = collect($product->warehouseInventories)->sum('qty') ?? 0;
-            $product['wishlist_item_count'] = collect($product->wishlistItems)->count() ?? 0;
-            $product['is_liked'] = Auth::check() ? in_array($this->customer()->_id, array_column($product->wishlistItems->toArray(), 'customer_id')) : false;
-            $product['first_product_variant'] = count($product['variants']) > 0 ? $product['variants'][0] : null;
-
-            if ($hasLocalDiscount) {
-                $price = $freeVariant['price'];
-                $selectedDiscount = $freeVariant['discounts'][0];
-
-                switch ($selectedDiscount['type']) {
-                    case ProductVariantDiscountType::PRICE:
-                        $price -= $selectedDiscount['value'];
-                        break;
-                    case ProductVariantDiscountType::PERCENTAGE:
-                        $price *= (1 - $selectedDiscount['value'] / 100);
-                        break;
-                    default:
-                        break;
-                }
-
-                $product['local_discount_type'] = $selectedDiscount['type'];
-                $product['discounted_price'] = strval($this->roundingValue($price, 0));
-            } else {
-                $product['local_discount_type'] = null;
-                $product['discounted_price'] = strval($product['price'] ?? 0);
-            }
-
-            // foreach ($hiddenKeys as $hiddenKey) {
-            //     unset($product[$hiddenKey]);
-            // }
-        }
-
         $hiddenKeys = [
             'discount',
             'remarks',
             'status',
             'is_system',
             'deleted_at',
-            'variants',
             'reviews',
             'warehouse_inventories',
             'wishlist_items'
         ];
-        $products = array_map(function ($product) use ($hiddenKeys) {
-            $key = array_search(0, array_column($product['variants'], 'price'));
-            if ($key != false) {
-                $variant = $product['variants'][$key];
-                $product['first_product_variant_id'] = $variant['_id'];
-                $product['price'] = $variant['price'];
-                $product['point'] = $variant['point'];
-            }
+
+        $products = Product::with([
+            'variants' => function ($productVariant) {
+                $productVariant
+                    ->statusActive()
+                    ->get();
+            },
+        ])
+            ->find($productIDs)
+            ->append(['first_product_variant_id', 'price', 'point']);
+
+        foreach ($products as $product) {
+            $product['local_discount_type'] = null;
+            $product['global_discount'] = null;
+            $product['rating'] = null;
+            $product['review_count'] = 0;
+            $product['inventory_count'] = 0;
+            $product['wishlist_item_count'] = 0;
+            $product['is_liked'] = false;
+            $product['discounted_price'] = strval($product['price'] ?? 0);
 
             foreach ($hiddenKeys as $hiddenKey) {
                 unset($product[$hiddenKey]);
             }
-            return $product;
-        }, $products->toArray());
+
+            foreach ($product['variants'] as $variant) {
+                $variant['inventory_count'] = collect($variant->warehouseInventories)->sum('qty') ?? 0;
+                unset($variant['warehouseInventories']);
+            }
+        }
 
         return $products;
     }
