92cf9da8f6bab79e9a943da02597c01212bd2580
Woo <aron_118@yahoo.com.hk>
2024-01-26 18:05:50 +0800

    customer getAllCustomerGroups returns assigned active customers

--- src/esgone/src/app/Http/Controllers/Customer/CustomerController.php
+++ src/esgone/src/app/Http/Controllers/Customer/CustomerController.php
@@ -72,7 +72,53 @@ class CustomerController extends Controller
             ->statusActive()
             ->where('is_system', false)
             ->whereNull('slug')
+            ->with([
+                'customers' => function ($customer) {
+                    $customer->whereIsDeleted(false);
+                },
+                'customers.account',
+                'customers.groups' => function ($group) {
+                    $group->where('is_system', false);
+                },
+            ])
             ->get();
+
+        $groups = $groups->toArray();
+        foreach ($groups as $groupKey => $group) {
+            foreach ($groups[$groupKey]['customers'] as $customerKey => $customer) {
+                $memberLevel = array_filter($groups[$groupKey]['customers'][$customerKey]['groups'], function ($group) {
+                    return $group['slug'] !== null;
+                });
+                $industries = array_filter($groups[$groupKey]['customers'][$customerKey]['groups'], function ($group) {
+                    return $group['slug'] === null;
+                });
+
+                $groups[$groupKey]['customers'][$customerKey]['user'] = [
+                    'username' => $groups[$groupKey]['customers'][$customerKey]['account']['username'],
+                    'avatar' => $groups[$groupKey]['customers'][$customerKey]['account']['avatar'],
+                ];
+                $groups[$groupKey]['customers'][$customerKey]['country'] = $groups[$groupKey]['customers'][$customerKey]['account']['country'];
+                $groups[$groupKey]['customers'][$customerKey]['gender'] = $groups[$groupKey]['customers'][$customerKey]['account']['gender'];
+                $groups[$groupKey]['customers'][$customerKey]['email'] = $groups[$groupKey]['customers'][$customerKey]['account']['email'];
+                $groups[$groupKey]['customers'][$customerKey]['area_code'] = $groups[$groupKey]['customers'][$customerKey]['account']['area_code'];
+                $groups[$groupKey]['customers'][$customerKey]['phone'] = $groups[$groupKey]['customers'][$customerKey]['account']['phone'];
+                $groups[$groupKey]['customers'][$customerKey]['company_name'] = $groups[$groupKey]['customers'][$customerKey]['account']['company_name'] ?? null;
+                // Tree
+                $groups[$groupKey]['customers'][$customerKey]['website'] = $groups[$groupKey]['customers'][$customerKey]['account']['website'] ?? null;
+                $groups[$groupKey]['customers'][$customerKey]['short_description'] = isset($groups[$groupKey]['customers'][$customerKey]['account']['short_description']) ?
+                    $groups[$groupKey]['customers'][$customerKey]['account']['short_description']
+                    : ['en' => null, 'zh' => null, 'cn' => null];
+                $groups[$groupKey]['customers'][$customerKey]['member_level'] = reset($memberLevel) ? reset($memberLevel)['slug'] : 'green-members';
+                $groups[$groupKey]['customers'][$customerKey]['industries'] = array_map(function ($industry) {
+                    return [
+                        '_id' => $industry['_id'],
+                        'title' => $industry['title'],
+                    ];
+                }, $industries);
+
+                unset($groups[$groupKey]['customers'][$customerKey]['account'], $groups[$groupKey]['customers'][$customerKey]['groups']);
+            }
+        }
 
         return $groups;
     }
