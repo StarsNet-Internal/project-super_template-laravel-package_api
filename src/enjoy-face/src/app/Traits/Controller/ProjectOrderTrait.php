4b233572765b2345e660b05f1f8a08de613eeaea
Woo <aron_118@yahoo.com.hk>
2024-06-21 16:57:33 +0800

    getOrderDetails receipt number -> cashier_id

--- src/enjoy-face/src/app/Traits/Controller/ProjectOrderTrait.php
+++ src/enjoy-face/src/app/Traits/Controller/ProjectOrderTrait.php
@@ -10,19 +10,22 @@ use Carbon\Carbon;
 
 trait ProjectOrderTrait
 {
-    private function getReceiptNumber(array $order, Builder $orders)
+    private function getReceiptNumber(array $order, array $orders)
     {
         $createdAt = Carbon::parse($order['created_at']);
         $startOfDay = $createdAt->copy()->setTimezone('Asia/Hong_Kong')->startOfDay();
 
         // Count the number of orders created on this day and before this order
-        $orderCount = $orders->where('created_at', '>=', $startOfDay)
-            ->where('created_at', '<=', $createdAt)
-            ->count();
+        $filteredOrders = array_filter($orders, function ($order) use ($createdAt, $startOfDay) {
+            return $startOfDay <= Carbon::parse($order['created_at']) && Carbon::parse($order['created_at']) < $createdAt;
+        });
+        // $orderCount = $orders->where('created_at', '>=', $startOfDay)
+        //     ->where('created_at', '<=', $createdAt)
+        //     ->count();
 
         // Generate the receipt number
         $datePart = $createdAt->format('Ymd');
-        $orderNumber = str_pad($orderCount, 4, '0', STR_PAD_LEFT);
+        $orderNumber = str_pad(count($filteredOrders), 4, '0', STR_PAD_LEFT);
         $receiptNumber = $datePart . $orderNumber;
 
         return $receiptNumber;
